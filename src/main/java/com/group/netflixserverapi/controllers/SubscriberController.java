5cb5a13bc0d095da6ccb3c5f0c171f9800c40975
milleradulu <milleradulu@gmail.com>
2019-10-31 04:10:03 +0300

    Define error handling for subscriber registration

--- src/main/java/com/group/netflixserverapi/controllers/SubscriberController.java
+++ src/main/java/com/group/netflixserverapi/controllers/SubscriberController.java
@@ -1,5 +1,8 @@
 package com.group.netflixserverapi.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.group.netflixserverapi.errors.ErrorFormatter;
 import com.group.netflixserverapi.models.Subscriber;
 import com.group.netflixserverapi.repositories.SubscriberRepository;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +17,7 @@ import java.util.Optional;
 public class SubscriberController {
 
     private final SubscriberRepository subscriberRepository;
+    private final ObjectMapper mapper = new ObjectMapper();
 
     public SubscriberController(SubscriberRepository subscriberRepository) {
         this.subscriberRepository = subscriberRepository;
@@ -21,22 +25,23 @@ public class SubscriberController {
 
     /**
      * To register new Subscribers
+     *
      * @param subscriber
      * @return String
      */
     @PostMapping
-    public String register(@RequestBody Subscriber subscriber) {
+    public String register(@RequestBody Subscriber subscriber) throws JsonProcessingException {
+
         Optional<Subscriber> newSubscriber = subscriberRepository.findByIdentificationNumber(subscriber.getIdentificationNumber());
+
         if (newSubscriber.isPresent())
-            return "Subscriber:" + subscriber.getIdentificationNumber() + " already registered.Cannot register again";
-        else {
-            if(subscriber.getIdentificationNumber()==null){
-                return "Identification number can not be null.Check identificationNumber: " + subscriber.getIdentificationNumber() ;
-            }
-            else{
-                subscriberRepository.save(subscriber);
-                return "Subscriber:" + subscriber.getIdentificationNumber() + " saved";
-            }
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(new ErrorFormatter("This subscriber exists"));
+        else if (subscriber.getIdentificationNumber() == null) {
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(new ErrorFormatter("identificationNumber cannot be null"));
         }
+
+        subscriberRepository.save(subscriber);
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(subscriber);
+
     }
 }
